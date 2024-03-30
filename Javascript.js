a692d0222445a7fd5ff86ea11daad2d390a310c3
Amjad <amjadalqurani2000.com>
2024-03-30 05:23:42 +0300

    Amjad

--- Javascript.js
+++ Javascript.js
@@ -11,7 +11,15 @@ humburger.onclick = function(){
     }
 }
 
-// ----------------------NavBar--------------------
+//Login And Signin Button
+document.querySelector('.Login-button').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+document.querySelector('.Signin-button').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+
+// ----------------------/NavBar--------------------
 
 
 // ----------------------Slider--------------------
@@ -45,9 +53,28 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
 
-// ----------------------Slider--------------------
+  document.querySelector('#oreder_btn').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+
+
+// ----------------------/Slider--------------------
+
+
+// ----------------------About--------------------
+
+document.querySelector('.hire__me').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+
+document.querySelector('.portfolio').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+
+// ----------------------/About--------------------
 
 // ----------------------Gallery--------------------
+
 document.querySelectorAll('.img_container img').forEach(drwa_image =>{
     drwa_image.onclick = () =>{
         document.querySelector('.Popup_Draw').style.display = 'block';
@@ -58,8 +85,56 @@ document.querySelectorAll('.img_container img').forEach(drwa_image =>{
 document.querySelector('.Popup_Draw span').onclick = () =>{
     document.querySelector('.Popup_Draw').style.display = 'none';
 }
-// ----------------------Gallery--------------------
+
+//See More Button 
+document.querySelector('.see_more_button').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+
+// ----------------------/Gallery--------------------
+
 
 // ----------------------Blogs--------------------
 
-// ----------------------Blogs--------------------
+document.querySelector('.see-more-btn').addEventListener('click', function() {
+    window.open('#', '_blank');
+});
+
+// ----------------------/Blogs--------------------
+
+// ----------------------links--------------------
+
+document.querySelector('.s_icon.fb').addEventListener('click', function() {
+    window.open('https://www.facebook.com/', '_blank');
+});
+document.querySelector('.s_icon.ig').addEventListener('click', function() {
+    window.open('https://www.instagram.com/', '_blank');
+});
+document.querySelector('.s_icon.lnk').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/feed/', '_blank');
+});
+document.querySelector('.s_icon.git').addEventListener('click', function() {
+    window.open('https://github.com/Amjadhatem', '_blank');
+});
+
+// ----------------------/links--------------------
+
+
+// ----------------------Q&A--------------------
+
+const items = document.querySelectorAll('.accordion button');
+function toggleAccordion(){
+    const itemToggle = this.getAttribute('aria-expanded');
+    
+    for(i =0 ; i<items.length ; i++){
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if(itemToggle == 'false'){
+        this.setAttribute('aria-expanded', 'true');
+    }
+}
+
+items.forEach(item =>item.addEventListener('click',toggleAccordion));
+
+// ----------------------/Q&A--------------------
